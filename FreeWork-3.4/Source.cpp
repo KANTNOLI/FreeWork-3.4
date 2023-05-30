db67cd8f9a3df8cf1657dc2b761452137c2ea675
KANTNOLI <zahargysev02@gmail.com>
2023-05-30 12:35:56 +0300

    Ready

--- FreeWork-3.4/Source.cpp
+++ FreeWork-3.4/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,6 +8,46 @@ using namespace std;
 //K, то вывести массив без изменений.
 
 int main() {
+	setlocale(LC_ALL, "RUS");
+	int input, size;
+	string msg = "Массив до: ";
+	do {
+		cout << "Введите целое число, которое больше нуля: ";
+		cin >> input;
+	} while (input <= 0);
+	input -= 1;
+
+	cout << "Введите размер массива: ";
+	cin >> size;
+
+	int* vector = new int[size];
+
+	cout << "Введите данные массива: ";
+	for (int i = 0; i < size; i++) {
+		cin >> vector[i];
+	}
+
+	system("cls");
+
+	for (int i = 0; i < size; i++)
+	{
+		msg += to_string(vector[i]) + " ";
+	}
+
+	msg += "\n";
+
+
+	if (input <= size) {
+		vector[input] = (input + 1);
+	}
+
+	msg += "Массив после: ";
+	for (int i = 0; i < size; i++)
+	{
+		msg += to_string(vector[i]) + " ";
+	}
+
+	cout << msg << endl;
 
 	return 0;
 }
